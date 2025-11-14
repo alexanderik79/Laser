2aa11ad60484c4599f9f865779ce886dd0cc729b
alexanderik79 <alexanderik79@gmail.com>
2025-11-14 08:38:48 +0100

    new strcuture

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 // src/js/main.js
-import { initI18n } from './i18n.js';
 import { initForm } from './form.js';
 import { initFaq } from './faq.js';
 import { initScroll } from './scroll.js';
 import { initAOS } from './aos.js';
 
 // Инициализация всех модулей
-initI18n();
 initForm();
 initFaq();
 initScroll();
